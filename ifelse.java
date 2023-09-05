35af0e559a01477c673e2a0aa38cbec10743fc66
Usama Zafar <90896659+Usama991@users.noreply.github.com>
2023-09-05 16:34:32 +0500

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse{
+    public static void main(String[] args) {
+        int Salary = 8000;
+        if (Salary > 10000){
+            Salary += 1000;
+        }   else if (  Salary < 10000){
+            Salary +=500;
+
+        }
+        System.out.println(Salary);
+
+    }
+
+}
